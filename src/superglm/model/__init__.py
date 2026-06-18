e218777633fd6d2ec92f1ab55659f39b5af54257
Max Hicks <158112925+StrudelDoodleS@users.noreply.github.com>
2026-06-18 20:37:49 +0100

    Install deployable fixed offset support

--- src/superglm/model/__init__.py
+++ src/superglm/model/__init__.py
@@ -2,7 +2,9 @@
 
 from superglm.model.api import SuperGLM
 from superglm.model.fit_ops import PathResult
+from superglm.offsets import install_deployable_offset_support
 
+install_deployable_offset_support(SuperGLM)
 SuperGLM.__module__ = "superglm.model"
 
 __all__ = ["SuperGLM", "PathResult"]
