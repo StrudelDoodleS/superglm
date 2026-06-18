08d659e904d1199bb61cd5d562f3dd9e93a2b7d2
Max Hicks <158112925+StrudelDoodleS@users.noreply.github.com>
2026-06-18 20:37:17 +0100

    Route rating export through fixed offset metadata

--- src/superglm/export/__init__.py
+++ src/superglm/export/__init__.py
@@ -1,5 +1,5 @@
 """Rating table export helpers."""
 
-from superglm.export.rating_tables import export_rating_tables
+from superglm.export.deployable_offsets import export_rating_tables
 
 __all__ = ["export_rating_tables"]
